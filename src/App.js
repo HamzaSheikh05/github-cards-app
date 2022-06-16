641c99948a23956979c26eedab79e7f5d4e3441f
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-16 20:18:41 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import AppShell from './Application';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello worlds</h1>
+      <AppShell/>
     </div>
   );
 }
