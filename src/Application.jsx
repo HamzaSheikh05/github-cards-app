cef35374067a978aa10a9f48f7dc00309898dd30
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-16 22:10:09 +0400

    setting state in form

--- src/Application.jsx
+++ src/Application.jsx
@@ -80,15 +80,21 @@ const CardList = (props) => {
 };
 
 class Form extends React.Component{
-    userNameInput = React.createRef();
+    state = {userName: ''}
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log(this.userNameInput.current.value);
+        console.log(this.state.userName);
     }
     render(){
         return(
             <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder='Github username' ref={this.userNameInput} required/>
+                <input 
+                type="text" 
+                placeholder='Github username'
+                value={this.state.userName} 
+                ref={this.userNameInput}
+                onChange={event => this.setState({userName: event.target.value})} 
+                required/>
                 <button>Add Card</button>
             </form>
         )
