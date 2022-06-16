617c32acb5e61fc9620b25a3f4eca5fa18004035
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-16 20:24:16 +0400

    created Card Component

---
+++ src/Application.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+class Card extends React.Component{
+    render(){
+        return(
+            <div className='github-profile'>
+
+            </div>
+        )
+    }
+}
+
+class AppShell extends React.Component{
+
+    render(){
+        return <div className='header'>The Github Cards App</div>
+    }
+}
+
+export default AppShell;
