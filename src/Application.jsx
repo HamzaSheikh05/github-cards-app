336090fe84ee1254df60dcbc247a1aa63ba28844
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-16 20:45:26 +0400

    Finished basic styling of card component

--- src/Application.jsx
+++ src/Application.jsx
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 class Card extends React.Component{
     render(){
         return(
-            <div className='github-profile'>
-
+            <div className='github-profile' style={{ margin: '1em' }}>
+                <img src='https://via.placeholder.com/75' style={{ width: '75px' }}/>
+                <div className="info" style={{ display: 'inline-block', marginleft: 10 }}>
+                    <div className="name" style={{ fontSize: '125%' }}>Name here...</div>
+                    <div className="company">Company here..</div>
+                </div>
             </div>
         )
     }
@@ -12,7 +16,12 @@ class Card extends React.Component{
 class AppShell extends React.Component{
 
     render(){
-        return <div className='header'>The Github Cards App</div>
+        return(
+            <div>
+                <div className='header'>The Github Cards App</div>
+                <Card/>
+            </div>
+        )
     }
 }
 
